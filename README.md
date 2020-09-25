9a84442e5955577aa03813bdd1cfbe3d03dc0913
candelario-dev <rebecca.candelario.dev@gmail.com>
2020-09-25 15:54:38 -0400

    Changed a few things

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-#Full Stack Homework Tracker
+# Full Stack Homework Tracker
 
 ![clock](https://user-images.githubusercontent.com/69063941/93758743-f9c17600-fbd6-11ea-97b2-80b9398a75da.png)
 
-###How it's made:
+### How it's made:
 HTML, EJS, Bootstrap, JavaScript, Node.js, MongoDb
+
+### How it works:
+You have so many assignments due that you can't keep track! Don't worry! The Full-Stack Homework tracker is here! Just input the assignment title, add the due date, and voila! See the countdown begin.
 
 
 ## Installation
---
+++ public/main.js
@@ -0,0 +1,72 @@
+
+loadEventListeners();
+
+function loadEventListeners() {
+	document.addEventListener('DOMContentLoaded', function() { calcTime(); });
+};
+
+var timeTo = document.getElementById('time-to').value,
+		date,
+		now = new Date(),
+		newYear = new Date('1.1.2020').getTime(),
+		startTimer = '';
+
+// calculate date, hour, minute and second
+function calcTime(dates) {
+	//ui variables
+	clearInterval(startTimer);
+
+	if(typeof(dates) == 'undefined'){
+		date = new Date(newYear).getTime();
+	}else {
+		date = new Date(dates).getTime();
+	}
+
+	function updateTimer(date){
+
+		var now = new Date().getTime();
+		var distance = date - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// select element
+		document.querySelector('.clock-day').innerHTML = days;
+		document.querySelector('.clock-hours').innerHTML = hours;
+		document.querySelector('.clock-minutes').innerHTML = minutes;
+		document.querySelector('.clock-seconds').innerHTML = seconds;
+
+		if(now >= date){
+			clearInterval(startTimer);
+			document.querySelector('.clock-day').innerHTML = 'T';
+			document.querySelector('.clock-hours').innerHTML = 'I';
+			document.querySelector('.clock-minutes').innerHTML = 'M';
+			document.querySelector('.clock-seconds').innerHTML = 'E';
+		}
+	}
+
+	startTimer = setInterval(function() { updateTimer(date); }, 1000);
+}
+
+let trash = document.getElementsByClassName("fa-trash");
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+    });
+});
